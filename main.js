6c12b6eed40a526090b9528eb56ac079e5436aa3
Markese Straughter <Kese08@hotmail.com>
2020-03-13 08:13:26 -0700

    activatedTransition

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-$('.box').on('click', function (e){
- if ($('p').hasClass('active')){
-     $('p').removeClass('active')
-}else{
-    $('p').addClass('active')
-}
-})
+$('#one').on('click', function (e){
+       $('.paragraph1').addClass('active') 
+       $('.paragraph2').removeClass('active')
+       $('.paragraph3').removeClass('active')
+   });
+
+$('#two').on('click', function (e){
+    $('.paragraph2').addClass('active')
+    $('.paragraph1').removeClass('active')
+    $('.paragraph3').removeClass('active')
+
+});
+
+$('#three').on('click', function (e){
+    $('.paragraph3').addClass('active')
+    $('.paragraph2').removeClass('active')
+    $('.paragraph1').removeClass('active')
+
+
+});
